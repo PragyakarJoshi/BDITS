e169ec2fcf584ecce6a8f05b320687d883f94e9a
Pragyakar <pragyakar75@gmail.com>
2018-01-23 12:12:23 +0545

    Model Binding

---
+++ SomeUniversity/SomeUniversity/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SomeUniversity.Startup))]
+namespace SomeUniversity
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
