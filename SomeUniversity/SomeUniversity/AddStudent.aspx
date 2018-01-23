19052641dc4dd4eef6292f7cb087d0ae07a9a013
Pragyakar <pragyakar75@gmail.com>
2018-01-23 12:50:14 +0545

    Dynamic Entity

---
+++ SomeUniversity/SomeUniversity/AddStudent.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="Add New Student" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AddStudent.aspx.cs" Inherits="SomeUniversity.AddStudent" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:ValidationSummary runat="server" ShowModelStateErrors="true" />
+    <asp:FormView runat="server" ID="addStudentForm"
+        ItemType="SomeUniversity.Models.Student" 
+        InsertMethod="addStudentForm_InsertItem" DefaultMode="Insert"
+        RenderOuterTable="false" OnItemInserted="addStudentForm_ItemInserted">
+        <InsertItemTemplate>
+            
+                <ol>
+                    <asp:DynamicEntity runat="server" Mode="Insert" />
+                </ol>
+                <asp:Button runat="server" Text="Insert" CommandName="Insert" CssClass="btn btn-success" />
+                <asp:Button runat="server" Text="Cancel" CausesValidation="false" OnClick="cancelButton_Click" CssClass ="btn btn-danger" />
+            
+        </InsertItemTemplate>
+    </asp:FormView>
+</asp:Content>
--- SomeUniversity/SomeUniversity/Students.aspx.cs
+++ SomeUniversity/SomeUniversity/Students.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using SomeUniversity.Models;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 
 namespace SomeUniversity
 {
@@ -22,5 +23,44 @@ namespace SomeUniversity
             var query = db.Students.Include(s => s.Enrollments.Select(e => e.Course));
             return query;
         }
+
+        
+        public void studentsGrid_UpdateItem(int studentID)
+        {
+           using (SchoolContext db = new SchoolContext())
+           {
+               Student item = null;
+               item = db.Students.Find(studentID);
+               if (item == null)
+               {
+                   ModelState.AddModelError("", String.Format("Item with id {0} was not found", studentID));
+                   return;
+               }
+               TryUpdateModel(item);
+               if (ModelState.IsValid)
+               {
+                   db.SaveChanges();
+               }
+           }
+        }
+
+        public void studentsGrid_DeleteItem(int studentID)
+        {
+            using(SchoolContext db = new SchoolContext())
+            {
+                var item = new Student { StudentID = studentID };
+                db.Entry(item).State = EntityState.Deleted;
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch(DbUpdateConcurrencyException)
+                {
+                    ModelState.AddModelError("", String.Format("Item with id {0} no longer exists in the database,", studentID));
+                }
+
+            }
+        }
+
     }
 }
