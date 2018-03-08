96ee7070aad1c98b8df4bb56f0610e0ba512145e
PragyakarJoshi <pragyakar75@gmail.com>
2018-03-08 11:45:22 +0545

    Added JouskaUniversity(MVC)

---
+++ JouskaUnivesrity/JouskaUnivesrity/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace JouskaUnivesrity
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ JouskaUnivesrity/JouskaUnivesrity/Models/Enrollment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JouskaUnivesrity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, E, F
+    }
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+        public virtual Course course { get; set; }
+        public virtual Student student { get; set; }
+    }
+}
